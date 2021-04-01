50a019ac7a58dbd4209c9fd3166326d9c80a15be
5arg <37589694+5arg@users.noreply.github.com>
2021-04-01 19:13:35 +0200

    default modals added

--- js/home.js
+++ js/home.js
@@ -26,3 +26,65 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = "block";
 }
+
+//modals
+document
+  .getElementById("italesse-modal-toggle")
+  .addEventListener("click", () => {
+    document.getElementById("italesse-modal").style.display = "block";
+  });
+
+document
+  .getElementById("italesse-modal-close")
+  .addEventListener("click", () => {
+    document.getElementById("italesse-modal").style.display = "none";
+  });
+
+document.getElementById("thrill-modal-toggle").addEventListener("click", () => {
+  document.getElementById("thrill-modal").style.display = "block";
+});
+
+document.getElementById("thrill-modal-close").addEventListener("click", () => {
+  document.getElementById("thrill-modal").style.display = "none";
+});
+
+document.getElementById("pivo-modal-toggle").addEventListener("click", () => {
+  document.getElementById("pivo-modal").style.display = "block";
+});
+
+document.getElementById("pivo-modal-close").addEventListener("click", () => {
+  document.getElementById("pivo-modal").style.display = "none";
+});
+
+document
+  .getElementById("hedonist-modal-toggle")
+  .addEventListener("click", () => {
+    document.getElementById("hedonist-modal").style.display = "block";
+  });
+
+document
+  .getElementById("hedonist-modal-close")
+  .addEventListener("click", () => {
+    document.getElementById("hedonist-modal").style.display = "none";
+  });
+
+window.onclick = function (event) {
+  switch (event.target) {
+    case document.getElementById("italesse-modal"): {
+      document.getElementById("italesse-modal").style.display = "none";
+      break;
+    }
+    case document.getElementById("thrill-modal"): {
+      document.getElementById("thrill-modal").style.display = "none";
+      break;
+    }
+    case document.getElementById("pivo-modal"): {
+      document.getElementById("pivo-modal").style.display = "none";
+      break;
+    }
+    case document.getElementById("hedonist-modal"): {
+      document.getElementById("hedonist-modal").style.display = "none";
+      break;
+    }
+  }
+};
